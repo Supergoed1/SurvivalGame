1e576bf9073d1cb756a11be6f7eef09a7af2a15c
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-03-09 18:29:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var power = 1;
 var zombiekills = 0;
+var notis = 0;
 
 function addNotification(message) {
     var noti = document.getElementById("notifications");
+    if(notis >= 8) {
+        noti.children[7].remove();
+        notis -= 1;
+    }
     var content = document.createElement('div');
     content.innerHTML = message;
     noti.appendChild(content);
+    notis += 1;
 }
