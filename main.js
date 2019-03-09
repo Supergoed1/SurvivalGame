161fdf483c326138de966e14921e6eeee294c3b3
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-03-09 18:21:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var zombiekills = 0;
 function addNotification(message) {
     var noti = document.getElementById("notifications");
     var content = document.createTextNode(message);
-    theDiv.appendChild(content);
+    noti.appendChild(content);
 }
